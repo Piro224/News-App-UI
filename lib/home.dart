a282be94aab7bc6e4724886d733a99c525921da5
Prosper Azimeh <azimehburna@gmail.com>
2022-11-30 02:41:26 -0800

    modified

--- lib/home.dart
+++ lib/home.dart
@@ -107,73 +107,73 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           children: [
             //stories
-            
-                     AnimatedOpacity(
-                    duration: const Duration(milliseconds: 200),
-                    opacity: closeTopContainer ? 0 : 1,
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      width: size.width,
-                      alignment: Alignment.topCenter,
-                      height: closeTopContainer ? 0 : 100,
-                      child: SizedBox(
-                        height: 80,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          physics: const BouncingScrollPhysics(),
-                          itemCount: stories.length,
-                          itemBuilder: (context, index) {
-                            return Stories(child: stories[index]);
-                          },
+
+            AnimatedOpacity(
+              duration: const Duration(milliseconds: 200),
+              opacity: closeTopContainer ? 0 : 1,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                width: size.width,
+                alignment: Alignment.topCenter,
+                height: closeTopContainer ? 0 : 100,
+                child: SizedBox(
+                  height: 80,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: stories.length,
+                    itemBuilder: (context, index) {
+                      return Stories(child: stories[index]);
+                    },
+                  ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 4,
+              child: Center(
+                child: ListView.builder(
+                  controller: controller,
+                  itemCount: _post.length,
+                  itemBuilder: (context, index) {
+                    double scale = 1.0;
+                    if (topContainer > 0.5) {
+                      scale = index + 0.5 - topContainer;
+                      if (scale < 0) {
+                        scale = 0;
+                      } else if (scale > 1) {
+                        scale = 1;
+                      }
+                    }
+                    return Opacity(
+                      opacity: scale,
+                      child: Transform(
+                        transform: Matrix4.identity()..scale(scale, scale),
+                        alignment: Alignment.bottomCenter,
+                        child: Align(
+                          heightFactor: 1.0,
+                          alignment: Alignment.topCenter,
+                          child: GestureDetector(
+                            onTap: () => Navigator.pushReplacement(context,
+                                MaterialPageRoute(
+                              builder: (context) {
+                                return ReadPage(
+                                  imagePath: _post[index],
+                                );
+                              },
+                            )),
+                            child: NewsCard(
+                              child: _post[index],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-
-           
-                   Expanded(
-                    flex: 4,
-                    child: Center(
-                      child: ListView.builder(
-                        controller: controller,
-                        itemCount: _post.length,
-                        itemBuilder: (context, index) {
-                          double scale = 1.0;
-                          if (topContainer > 0.5) {
-                            scale = index + 0.5 - topContainer;
-                            if (scale < 0) {
-                              scale = 0;
-                            } else if (scale > 1) {
-                              scale = 1;
-                            }
-                          }
-                          return Opacity(
-                            opacity: scale,
-                            child: Transform(
-                              transform: Matrix4.identity()
-                                ..scale(scale, scale),
-                              alignment: Alignment.bottomCenter,
-                              child: Align(
-                                heightFactor: 1.0,
-                                alignment: Alignment.topCenter,
-                                child: GestureDetector(
-                                  onTap: () => Navigator.pushReplacement(
-                                      context, MaterialPageRoute(
-                                    builder: (context) {
-                                      return  ReadPage(imagePath: _post[index],);
-                                    },
-                                  )),
-                                  child: NewsCard(
-                                    child: _post[index],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
+                    );
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
